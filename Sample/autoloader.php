a93f1c7ec78a51dce3443034d4188a97b54b1cd8
Jordi Jolink <info@soneritics.nl>
2016-06-16 21:14:36 +0200

    Remove sample files
    Moved to a new repository
    Link can be found in the README.md file

--- Sample/autoloader.php
+++
@@ -1,7 +0,0 @@
-<?php
-spl_autoload_register(function ($className) {
-    $file = __DIR__ . '/../src/Soneritics/' . str_replace('\\', '/', $className) . '.php';
-    if (file_exists($file)) {
-        include $file;
-    }
-});
--- Sample/configs.php
+++
@@ -1,4 +0,0 @@
-<?php
-$websiteKey = ''; // Get this at the Buckaroo Payment Plaza
-$secretKey = ''; // Get this at the Buckaroo Payment Plaza
-$returnURL = ''; // Full URL to the ReturnPage.php script
